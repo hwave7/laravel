a95dad3db25e4e918a90d56caf8b696ae7c45f02
hwave7 <hwave7@gmail.com>
2022-04-01 10:16:59 +0900

    laravel2 20220401 1016

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>My First Page</title>
+</head>
+<body>
+  <p>{{$name}}さん</p>
+  <p>{{$message}}</p>
+  <p>現在は {{$today}} です</p>
+<p>Apr/01/2022</p>
+</body>
+</html>
